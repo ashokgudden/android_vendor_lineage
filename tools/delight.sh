e1e0aeb859776609e5c01504f35516d1f5b876ee
Ashok Soni <ashok.soni@gmail.com>
2017-10-28 15:16:03 +0530

    Few bash scripts.
    Change-Id: I53dfb957d9dea15c312d6c466b7c4abe7b9dd9fb

---
+++ tools/delight.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Root method chooser for the Resurrection Remix ROM
+# Specify colors utilized in the terminal
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    ylw=$(tput setaf 3)             #  yellow
+    blu=$(tput setaf 4)             #  blue
+    ppl=$(tput setaf 5)             #  purple
+    cya=$(tput setaf 6)             #  cyan
+    txtbld=$(tput bold)             #  Bold
+    bldred=${txtbld}$(tput setaf 1) #  red
+    bldgrn=${txtbld}$(tput setaf 2) #  green
+    bldylw=${txtbld}$(tput setaf 3) #  yellow
+    bldblu=${txtbld}$(tput setaf 4) #  blue
+    bldppl=${txtbld}$(tput setaf 5) #  purple
+    bldcya=${txtbld}$(tput setaf 6) #  cyan
+    txtrst=$(tput sgr0)             #  Reset
+    rev=$(tput rev)                 #  Reverse color
+    pplrev=${rev}$(tput setaf 5)
+    cyarev=${rev}$(tput setaf 6)
+    ylwrev=${rev}$(tput setaf 3)
+    blurev=${rev}$(tput setaf 4)
+    normal='tput sgr0'
+
+echo -e "";
+echo -e "";
+echo -e ${ppl}"88888888ba                                                                                 "${txtrst};
+echo -e ${ppl}"88      88b                                                                                "${txtrst};
+echo -e ${ppl}"88      ,8P                                                                                "${txtrst};
+echo -e ${blu}"88aaaaaa8P'   ,adPPYba,                                                                    "${txtrst};
+echo -e ${blu}"888888888b,  a8P_____88                                                                    "${txtrst};
+echo -e ${blu}"88      '8b  8PP8888888                                                                    "${txtrst};
+echo -e ${ylw}"88      a8P  88b,   ,aa                                                                    "${txtrst};
+echo -e ${ylw}"88888888P8   '8Ybbd88'                                                                    "${txtrst};
+echo -e "";
+echo -e "";
+echo -e ${ppl}"88888888ba,                88  88               88                                       88"${txtrst};
+echo -e ${ppl}"88      '88b               88  88               88            ,d                         88"${txtrst};
+echo -e ${ppl}"88        '8b              88                   88            88                         88"${txtrst};
+echo -e ${ppl}"88         88   ,adPPYba,  88  88   ,adPPYb,d8  88,dPPYba,  MM88MMM  ,adPPYba,   ,adPPYb,88"${txtrst};
+echo -e ${grn}"88         88  a8P_____88  88  88  a88    'Y88  88P'    88a   88    a8P_____88  a88    'Y88"${txtrst};
+echo -e ${grn}"88         8P  8PP8888888  88  88  8b       88  88       88   88    8PP8888888  8b       88"${txtrst};
+echo -e ${grn}"88      .a8P   88b,   ,aa  88  88  88a,   ,d88  88       88   88,   88b,   ,aa  88a,   ,d88"${txtrst};
+echo -e ${ylw}"88888888Y8'     '8Ybbd88'  88  88   '8YbbdP8Y8  88       88   8Y888  '8Ybbd88'   '88bbdP8Y8"${txtrst};
+echo -e ${ylw}"                                    aa,    ,88                                             "${txtrst};
+echo -e ${ylw}"                                     8Y8bbdP8                                              "${txtrst};
+echo -e "";
+echo -e "";
+echo -e ${ylw}" √ Build is Successfully Done! "${txtrst};
+echo -e "";
+echo -e ${blu}" 〉Powered by ANDROID™ OREO"${txtrst};
+echo -e "";
+echo -e ${ylw}" 〉BE-DELIGHTED ALWAYS "${txtrst};
+echo -e "";
